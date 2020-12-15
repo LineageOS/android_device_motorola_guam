9f5f5748c02eb00d58c15e89f3a6abb76ce7eaa3
Alessandro Astone <ales.astone@gmail.com>
2020-12-15 01:20:29 +0100

    guam: Use libcomparetf2 shim for __lttf2
    Change-Id: I5f71840886e0ddf1fa15a40f4d56dedb3fead22e

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libvidhance.so)
+            [ "$2" = "" ] && return 0
+            grep -q libcomparetf2_shim.so "${2}" || "${PATCHELF}" --add-needed libcomparetf2_shim.so "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
